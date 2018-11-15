b63745725c3177c0bdf97cfc405c8b1da93ff5d9
jayaalipuria <43646976+jayaalipuria@users.noreply.github.com>
2018-11-15 12:46:26 +0530

    updated generic views

--- polling/urls.py
+++ polling/urls.py
@@ -13,6 +13,6 @@ app_name= 'polling'
 urlpatterns[
    path('',views.IndexView.as_view(), name='index'),
    path('<int:pk>/',views.DetailView.as_view(), name='detail'),
-   path('<int:pk>/vote/',views.VoteView.as_view(), name='vote'),
-   path('<int:question_id>/results/',views.results, name='results'), 
+   path('<int:question_id>/vote/',views.votes, name='vote'),
+   path('<int:pk>/results/',views.ResultsView.as_view(), name='results'), 
 ]
