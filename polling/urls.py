8495ee692e13310938f760b56ca36b0a71165140
jayaalipuria <43646976+jayaalipuria@users.noreply.github.com>
2018-11-15 11:22:03 +0530

    Adding generic views

--- polling/urls.py
+++ polling/urls.py
@@ -3,9 +3,16 @@ from . import views
 
 app_name= 'polling'
 
-urlpatterns = [
-    path('',views.index, name='index'),
-    path('<int:question_id>/',views.detail, name='detail'),
-    path('<int:question_id>/vote/',views.vote, name='vote'),
-    path('<int:question_id>/results/',views.results, name='results'),
+#urlpatterns = [
+#    path('',views.index, name='index'),
+#    path('<int:question_id>/',views.detail, name='detail'),
+#    path('<int:question_id>/vote/',views.vote, name='vote'),
+#    path('<int:question_id>/results/',views.results, name='results'),
+#]
+
+urlpatterna[
+   path('',views.IndexView.as_view(), name='index'),
+   path('<int:pk>/',views.DetailView.as_view(), name='detail'),
+   path('<int:pk>/vote/',views.VoteView.as_view(), name='vote'),
+   path('<int:question_id>/results/',views.results, name='results'), 
 ]
